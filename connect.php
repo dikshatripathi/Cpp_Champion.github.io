5880ab42f2aff950f93399ebc61c53144dad59b4
Diksha Tripathi <dikshatripathi024@gmail.com>
2023-03-21 15:31:48 +0530

    connect added

--- connect.php
+++ connect.php
@@ -1,24 +1,22 @@
 <?php
-echo "hey this is working";
-$name=$_POST['name'];
-$phone=$_POST['phone'];
-$email=$_POST['email'];
-$message=$_POST['message'];
+echo "hey";
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$message = $_POST['message'];
 
 
- //Database connection
- $conn =new mysqli("localhost","root,","","test");
- if($conn->connect){
-    
-      die('Connection Failed : '.$conn->connect_error);   
+$conn = new mysqli('localhost','root','','form');
+if($conn->connect_error)
+{
+  die('Connection Failed :'.$conn->connect_error);
+
+}else{
+  $stmt = $conn->prepare("insert into register(name,phone,email,message) value(?,?,?,?)");
+  $stmt->bind_param("siss",$name,$phone,$email,$message);
+  $stmt->execute();
+  echo "registration Successfully";
+  $stmt->close();
+  $conn->close();
 }
- else{
-    echo "Error";
-    $stmt=$conn->prepare("insert into registration (name,phone,email,message) values(?,?,?,?)");
-    $stmt->bind_param("siss",$name,$phone,$email,$message);
-    $stmt->execute();
-    echo "registration Successfully.......";
-    $stmt->close();
-    $conn->close();
- }
 ?>
